0e0664d3cce85bc6d72bd2cdb9f8bb0fb1c3c15a
Ugdha <alexisvlrt@gmail.com>
2019-03-01 00:28:09 +0100

    Updated dockerfile and fixed file upload though the API

--- docker/API.py
+++ docker/API.py
@@ -7,10 +7,8 @@ import Strategy
 
 @route('/upload', method='POST')
 def ingest():
-    script = request.files.get('script').file.getvalue()
-    data = pickle.loads(request.files.get('data').file.getvalue())
-    with open('Strategy.py', 'wb') as f:
-        f.write(script)
+    request.files.get('script').save('Strategy.py', overwrite=True)
+    data = pickle.loads(request.files.get('data').file.read())
 
     reload(Strategy)
     return Strategy.Strategy(data).run()
