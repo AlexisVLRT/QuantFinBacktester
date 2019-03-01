ee42417ddb4acfb9ede199420f98cf51a663f6ac
avialaret <alexisvlrt@gmail.com>
2019-03-01 23:58:09 +0100

    Less requirements, lighter image

--- MapReduce.py
+++ MapReduce.py
@@ -6,11 +6,11 @@ import time
 
 
 class Mapper:
-    def __init__(self, tasks, reducer, slaves_count, host, port):
+    def __init__(self, tasks, slaves_count, host, port):
         self.swarm_host = host
         self.swarm_port = port
         self.tasks = tasks
-        self.reducer = reducer
+        self.reducer = Reducer(self.swarm_host, self.swarm_port)
         self.slave_count = slaves_count
         self.tasks_in_progress = 0
         self.run()
@@ -30,16 +30,29 @@ class Mapper:
             time.sleep(0.1)
 
     def post_task(self, task_data):
+        ticker = task_data
         print('Posting task')
-        r = requests.post('http://{}:{}/upload'.format(self.swarm_host, self.swarm_port), files={'script': open('Strategy.py', 'rb'), 'data': open('Data/{}_5.pickle'.format(task_data), 'rb')})
+        r = requests.post('http://{}:{}/upload'.format(self.swarm_host, self.swarm_port), files={'script': open('Strategy.py', 'rb'), 'data': open('Data/{}_5.pickle'.format(ticker), 'rb')})
         print(r.text)
-        self.reducer.add_result(r.json())
+        self.reducer.wait_for_result(ticker)
         self.tasks_in_progress -= 1
 
 
 class Reducer:
-    def __init__(self):
+    def __init__(self, host, port):
+        self.swarm_host = host
+        self.swarm_port = port
         self.raw_results = []
+
+    def wait_for_result(self, task):
+        done = False
+        while not done:
+            r = requests.get('http://{}:{}/result'.format(self.swarm_host, self.swarm_port))
+            if 'response' in r.json().keys() and r.json()['response'] == 200:
+                done = True
+                self.raw_results = r.json()['data']
+            else:
+                time.sleep(0.1)
 
     def add_result(self, result):
         self.raw_results.append(result)
@@ -47,7 +60,8 @@ class Reducer:
 
 if __name__ == '__main__':
     filenames = os.listdir('Data')
-    tasks = [name.split('_')[0] for name in filenames]
+    tasks = [name.split('_')[0] for name in filenames][:1]
     print(tasks)
     swarm_host, swarm_port = '149.91.83.188', 80
-    Mapper(tasks, Reducer(), 7, swarm_host, swarm_port)
+    # swarm_host, swarm_port = 'localhost', 9999
+    Mapper(tasks, 1, swarm_host, swarm_port)
