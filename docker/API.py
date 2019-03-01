ae30275134c1e38fb342da3f60432b6ef7a35fb7
avialaret <alexisvlrt@gmail.com>
2019-03-01 10:01:30 +0100

    API returns traceback in the event of a server internal error

--- docker/API.py
+++ docker/API.py
@@ -1,17 +1,21 @@
 from bottle import route, run, request
 from importlib import reload
 import pickle
+import traceback
 
 import Strategy
 
 
 @route('/upload', method='POST')
 def ingest():
-    request.files.get('script').save('Strategy.py', overwrite=True)
-    data = pickle.loads(request.files.get('data').file.read())
+    try:
+        request.files.get('script').save('Strategy.py', overwrite=True)
+        data = pickle.loads(request.files.get('data').file.read())
 
-    reload(Strategy)
-    return Strategy.Strategy(data).run()
+        reload(Strategy)
+        return Strategy.Strategy(data).run()
+    except Exception as e:
+        return {'error': traceback.format_exc()}
 
 
 if __name__ == '__main__':
