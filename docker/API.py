759786fca701eec64c236911412ab9dded0b35cf
Ugdha <alexisvlrt@gmail.com>
2019-03-02 18:49:55 +0100

    API will not accept new tasks while already executing one

--- docker/API.py
+++ docker/API.py
@@ -14,8 +14,12 @@ def ingest(task='task'):
         request.files.get('script').save('Strategy.py', overwrite=True)
         data = pickle.loads(request.files.get('data').file.read())
 
-        result = 202
-        Thread(target=run_strategy, args=(task, data)).start()
+        if result == 202:
+            return {'response': 400}
+        else:
+            result = 202
+            Thread(target=run_strategy, args=(task, data)).start()
+            return {'response': 202}
 
     except Exception as e:
         return {'error': traceback.format_exc()}
