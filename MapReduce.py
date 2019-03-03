e1c9a6a8cb8fff27db8c306b25dc0ae9d2aa1da7
Ugdha <alexisvlrt@gmail.com>
2019-03-04 00:47:34 +0100

    Changed basically everything

--- MapReduce.py
+++ MapReduce.py
@@ -1,4 +1,3 @@
-import pickle
 import requests
 import os
 from threading import Thread
@@ -6,62 +5,98 @@ import time
 
 
 class Mapper:
-    def __init__(self, tasks, slaves_count, host, port):
+    def __init__(self, tasks, host, port):
         self.swarm_host = host
         self.swarm_port = port
         self.tasks = tasks
         self.reducer = Reducer(self.swarm_host, self.swarm_port)
-        self.slave_count = slaves_count
-        self.tasks_in_progress = 0
+        self.ack_flag = 0
         self.run()
 
-        while self.tasks_in_progress:
+        while self.reducer.tasks_in_progress:
             time.sleep(0.1)
         print(self.reducer.raw_results)
-        with open('results', 'w') as f:
-            f.writelines(str(self.reducer.raw_results))
 
     def run(self):
         while len(tasks):
-            if self.tasks_in_progress < self.slave_count:
+            if len(self.reducer.nodes):
+                self.ack_flag = 0
                 Thread(target=self.post_task, args=(tasks[0], )).start()
-                self.tasks_in_progress += 1
-                del tasks[0]
+                while not self.ack_flag:
+                    time.sleep(0.1)
+                print('Tasks running :', self.reducer.tasks_in_progress)
+                print('Tasks queued :', len(tasks))
+                time.sleep(0.5)
             time.sleep(0.1)
 
     def post_task(self, task_data):
         ticker = task_data
-        print('Posting task')
-        r = requests.post('http://{}:{}/upload'.format(self.swarm_host, self.swarm_port), files={'script': open('Strategy.py', 'rb'), 'data': open('Data/{}_5.pickle'.format(ticker), 'rb')})
-        print(r.text)
-        self.reducer.wait_for_result(ticker)
-        self.tasks_in_progress -= 1
+        print('Posting task :', task_data)
+        r = requests.post('http://{}:{}/upload/{}'.format(self.swarm_host, self.swarm_port, task_data), files={'script': open('Strategy.py', 'rb'), 'data': open('Data/{}_5.pickle'.format(ticker), 'rb')})
+        if 'response' in r.json().keys():
+            if r.json()['response'] == 202:
+                print('Ok')
+                del tasks[0]
+                self.ack_flag = 1
+                self.reducer.tasks_in_progress = self.reducer.nodes_count - len(self.reducer.nodes) + 1
+                self.reducer.nodes.remove(r.json()['id'])
+            elif r.json()['response'] == 400:
+                print('Failed')
+                self.ack_flag = -1
+
+        if not self.reducer.is_running:
+            Thread(target=self.reducer.wait_for_result).start()
 
 
 class Reducer:
     def __init__(self, host, port):
         self.swarm_host = host
         self.swarm_port = port
-        self.raw_results = []
+        self.raw_results = {}
+        self.is_running = False
+        self.tasks_in_progress = 0
+        self.nodes = self.get_nodes()
+        self.nodes_count = len(self.nodes)
 
-    def wait_for_result(self, task):
+    def wait_for_result(self):
+        print('Reducer Started')
+        self.is_running = True
         done = False
         while not done:
             r = requests.get('http://{}:{}/result'.format(self.swarm_host, self.swarm_port))
+            print(r, r.text)
             if 'response' in r.json().keys() and r.json()['response'] == 200:
-                done = True
-                self.raw_results = r.json()['data']
+                print('Received result', r.json())
+                self.raw_results.update(r.json()['data'])
+                self.nodes.add(r.json()['id'])
+                self.tasks_in_progress = self.nodes_count - len(self.nodes) + 1
+                print('Tasks running :', self.tasks_in_progress)
+                with open('results', 'w') as f:
+                    f.writelines(str(self.raw_results))
             else:
                 time.sleep(0.1)
 
-    def add_result(self, result):
-        self.raw_results.append(result)
+            if self.tasks_in_progress == 0:
+                done = True
+        print('Reducer Done')
+
+    def get_nodes(self):
+        ids = set([])
+        i, supposed_n_nodes = 0, 2**16
+        while i < supposed_n_nodes * 3:
+            r = requests.get('http://{}:{}/ping'.format(self.swarm_host, self.swarm_port))
+            node_id = r.json()['id']
+            if node_id in ids:
+                supposed_n_nodes = len(ids)
+            ids.add(node_id)
+            i += 1
+        return ids
 
 
 if __name__ == '__main__':
     filenames = os.listdir('Data')
-    tasks = [name.split('_')[0] for name in filenames][:1]
+    tasks = [name.split('_')[0] for name in filenames][:200]
     print(tasks)
     swarm_host, swarm_port = '149.91.83.188', 80
     # swarm_host, swarm_port = 'localhost', 9999
-    Mapper(tasks, 1, swarm_host, swarm_port)
+    Mapper(tasks=tasks, host=swarm_host, port=swarm_port)
