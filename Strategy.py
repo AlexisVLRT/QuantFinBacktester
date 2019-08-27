c2dfa3ec93e9a91135fc383be82e0a76d5e812a6
avialaret <alexisvlrt@gmail.com>
2019-08-27 17:21:53 +0200

    Updated strategy

--- Strategy.py
+++ Strategy.py
@@ -37,6 +37,8 @@ class Strategy:
         for i in range(self.start_offset, self.start_offset + self.n_samples):
             if not i % 100:
                 print(i, len(self.data))
+            if self.start_offset > len(self.data):
+                break
             self.current_timestamp = self.data.index[i]
             ema24 = self.data.loc[:self.current_timestamp].ewm(span=26 * self.day_length).mean()
             ema12 = self.data.loc[:self.current_timestamp].ewm(span=12 * self.day_length).mean()
@@ -45,47 +47,27 @@ class Strategy:
             diff = pd.Series(((macd - signal)/(macd - signal).std()).values[:, 0])
             diff2 = (diff.fillna(0).diff().ewm(span=0.5 * self.day_length).mean()).fillna(0) * 200
 
-            # Buy when diff > 0 and diff2 goes > 0
-            if self.position == 0:
-                if diff.iloc[-1] > 0 and diff2.iloc[-1] > 0 > diff2.iloc[-2]:
-                    self.buy()
+            # Buy when diff < 0 and diff2 goes < 0
+            if diff.iloc[-1] < 0 and diff2.iloc[-1] < 0 < diff2.iloc[-2]:
+                self.buy()
 
-            # short when diff < 0 and diff2 goes < 0
-            if self.position == 0:
-                if diff.iloc[-1] < 0 and diff2.iloc[-1] < 0 < diff2.iloc[-2]:
-                    self.short()
+            # Sell when diff goes > 0
+            if diff.iloc[-1] > 0 > diff.iloc[-2]:
+                self.sell()
 
-            # Sell as soon as we achieve a sufficient year-based profit or after three days if we lose too much
-            if type(self.position) is tuple and self.position[0] == 1:
-                open_date = self.position[1]
-                price_in = self.data.loc[open_date, 'close']
-                y_profit = 1
-                n_ticks = i - list(self.data.index).index(open_date)
+            # short when diff > 0 and diff2 goes > 0
+            if diff.iloc[-1] > 0 and diff2.iloc[-1] > 0 > diff2.iloc[-2]:
+                self.short()
 
-                thresh_profit = y_profit * self.period * price_in / (480 * 252) * n_ticks + price_in + price_in * 0.01
-                thresh_loss = -y_profit * self.period * price_in / (480 * 252) * n_ticks + price_in - price_in * 0.01
-
-                if self.data.iloc[i, 0] < thresh_loss or diff.iloc[-2] < 0 < diff.iloc[-1] or self.data.iloc[i, 0] > thresh_profit:
-                    self.sell()
-
-            # Cover as soon as we achieve a sufficient year-based profit or after three days if we lose too much
-            if type(self.position) is tuple and self.position[0] == -1:
-                open_date = self.position[1]
-                price_in = self.data.loc[open_date, 'close']
-                y_profit = 1
-                n_ticks = i - list(self.data.index).index(open_date)
-
-                thresh_profit = -y_profit * self.period * price_in / (480 * 252) * n_ticks + price_in - price_in * 0.01
-                thresh_loss = y_profit * self.period * price_in / (480 * 252) * n_ticks + price_in + price_in * 0.01
-
-                if self.data.iloc[i, 0] > thresh_loss or diff.iloc[-2] > 0 > diff.iloc[-1] or self.data.iloc[i, 0] < thresh_profit:
-                    self.cover()
+            # Cover when diff goes < 0
+            if diff.iloc[-1] < 0 < diff.iloc[-2]:
+                self.cover()
         return self.calls
 
 
 if __name__ == '__main__':
-    ticker = 'ADBE'
+    ticker = 'STNE'
     data = pickle.load(open('Data/{}_5.pickle'.format(ticker), 'rb'))
-    strat = Strategy(data)
+    strat = Strategy(data, 500, 1000)
     calls = strat.run()
     print(calls)
