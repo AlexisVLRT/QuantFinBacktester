f6bcc33776b8dfc9ef78d67b278b762294d4e3c2
avialaret <alexisvlrt@gmail.com>
2019-08-27 17:19:43 +0200

    Fixed bugs for the final simulation recap

--- FinalSim.py
+++ FinalSim.py
@@ -27,27 +27,39 @@ print('Done')
 
 initial_cash, initial_invested, initial_total = 1, 0, 1
 cash, invested, total = initial_cash, initial_invested, initial_total
-inv_fraction = 0.02
+inv_fraction = 0.1
 open_positions = {}
 for timestamp, calls in ordered_calls.iterrows():
     for ticker, call in calls[0]:
-        # print(timestamp, call)
-        print(call, ticker)
-        if call in ['buy', 'short'] and inv_fraction * initial_cash <= cash:
-            open_positions[ticker] = stocks_data.loc[timestamp, ticker]
-            invested += inv_fraction * initial_cash
-            cash -= inv_fraction * initial_cash
+        acted = False
+        if call in ['buy', 'short'] and cash >= inv_fraction * max(initial_cash, cash):
+            open_positions[ticker] = {
+                'entry_price': stocks_data.loc[timestamp, ticker],
+                'invested': inv_fraction * max(initial_cash, cash)
+            }
+
+            invested += inv_fraction * max(initial_cash, cash)
+            cash -= inv_fraction * max(initial_cash, cash)
+            acted = True
 
         elif call in ['sell', 'cover'] and ticker in open_positions.keys():
-            entry_price = open_positions[ticker]
+            entry_price = open_positions[ticker]['entry_price']
             exit_price = stocks_data.loc[timestamp, ticker]
+
             if call == 'sell':
                 profit = exit_price / entry_price - 1
-
             else:
                 profit = 1 - exit_price / entry_price
 
-            invested -= inv_fraction * initial_cash
-            cash += (inv_fraction * initial_cash) * (profit + 1)
+            investment_value = open_positions[ticker]['invested'] * (profit + 1)
+
+            invested -= open_positions[ticker]['invested']
+            cash += open_positions[ticker]['invested'] * (profit + 1)
             total = invested + cash
-        print(cash, invested, total)
+            del open_positions[ticker]
+            acted = True
+
+        if acted:
+            print(call, ticker)
+            print(f'Cash: {cash}\nInvested: {invested}\nTotal: {total}\n\n')
+
